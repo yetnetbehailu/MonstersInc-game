204907d24373c2100fed23034f6aca1441b85754
yetnetbehailu <yetnet.behailu@hotmail.com>
2021-01-17 17:01:39 +0000

    instructions modal created with js function to open/close on btn click(opt close click outside modal or on x mark btn with hover effect).Styling basic/centered/inc.icon

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,3 +4,32 @@
         let audio = document.getElementById("audio");
         return audio.paused ? audio.play() : audio.pause();
 };
+
+//How to play -Modal code sourced from Url (https://www.youtube.com/watch?v=6ophW7Ask_0)
+var customModal = document.getElementById("instructionsmodal");
+
+var customBtn = document.getElementById("customBtn");
+
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+
+customBtn.addEventListener("click", openModal);
+
+closeBtn.addEventListener("click", closeModal);
+
+
+window.addEventListener("click", outsideClick);
+
+function openModal(){
+    instructionsmodal.style.display = "block";
+}
+
+function closeModal(){
+    instructionsmodal.style.display = "none";
+}
+
+function outsideClick(e) {
+    if (e.target == instructionsmodal){
+        instructionsmodal.style.display = "none";
+    }
+}
+
