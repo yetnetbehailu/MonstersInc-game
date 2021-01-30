b5c2006f09b114a9dd9457e6dba10bfb6a3f7a9a
yetnetbehailu <yetnet.behailu@hotmail.com>
2021-01-30 00:00:20 +0000

    Card flipp function modified;named,toggle removed,condition statement & data attr/property added

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,23 @@
 // card deck flipp effect sourced https://3dtransforms.desandro.com/card-flip..
 
 var cards = document.querySelectorAll('.card');
-cards.forEach(card => { card.addEventListener('click', function(){
-    card.classList.toggle('is-flipped');
-})  
+let isCardFlipped = false;
+let firstCard, secondCard;
+cards.forEach(card => {
+card.addEventListener('click', function cardFlipped() {
+        this.classList.add('is-flipped');
+        if (!isCardFlipped) {
+            isCardFlipped = true;
+            firstCard = this;
+            return;
+        } 
+            secondCard = this;
+            isCardFlipped = false;
+            checkForMatch();
+    });
 });
+//check for match...
+function checkForMatch() {
+    let matchMade = firstCard.dataset.character === secondCard.dataset.character;
+    matchMade ? hideCards(): unflipCards();
+}
